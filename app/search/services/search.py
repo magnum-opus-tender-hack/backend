17e1c066a4dd1e46c91c00b74c094b1d9fef34de
ilia <il.vsl0110@gmail.com>
2022-10-21 23:22:14 +0300

    add hint schema

--- app/search/services/search.py
+++ app/search/services/search.py
@@ -1,5 +1,6 @@
 from search.models import Product
+from typing import List
 
 
-def process_string(text: str) -> [dict]:
+def process_string(text: str) -> List[dict]:
     return [x.serialize_self() for x in Product.objects.filter(name__contains=text)[5:]]
