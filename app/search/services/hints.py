dff46d371757f7ef60009ee8db60dd8c43ac492e
Alexandr Karpov <alexandr.d.karpov@gmail.com>
2022-10-22 18:21:42 +0300

    fixed hints

--- app/search/services/hints.py
+++ app/search/services/hints.py
@@ -2,7 +2,7 @@ from search.models import Product, Category, Characteristic
 
 
 def get_hints(content: str) -> str:
-    category = "Unknown"
+    category = "All"
     if content in list(map(lambda product: product.name, Product.objects.all())):
         category = "Name"
     elif content in list(map(lambda category: category.name, Category.objects.all())):
