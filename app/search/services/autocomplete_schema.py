23e30bbcb828b225bdcf39fdced08cb66614336c
Alexandr Karpov <alexandr.d.karpov@gmail.com>
2022-10-22 12:29:51 +0300

    improved autocomplete

--- app/search/services/autocomplete_schema.py
+++ app/search/services/autocomplete_schema.py
@@ -2,37 +2,38 @@ from search.models import Product, Category, Characteristic
 
 
 def autocomplete_schema(val: str):
-    schema = []
+    schema = [
+        {
+            "coordinate": product["name"].lower().index(val.lower()),
+            "value": {
+                "type": "Name",
+                "value": product["name"],
+            },
+        }
+        for product in Product.objects.filter(name__unaccent__icontains=val).values(
+            "name"
+        )
+    ]
     schema.extend(
         [
             {
-                "coordinate": product["name"].index(val),
-                "value": {
-                    "type": "Name",
-                    "value": product["name"],
-                },
-            }
-            for product in Product.objects.filter(name__contains=val).values("name")
-        ]
-    )
-    schema.extend(
-        [
-            {
-                "coordinate": cat["name"].index(val),
+                "coordinate": cat["name"].lower().index(val.lower()),
                 "value": {"type": "Category", "value": cat["name"]},
             }
-            for cat in Category.objects.filter(name__contains=val).values("name")
+            for cat in Category.objects.filter(name__unaccent__icontains=val).values(
+                "name"
+            )
         ]
     )
     schema.extend(
         [
             {
-                "coordinate": char["name"].index(val),
+                "coordinate": char["value"].lower().index(val.lower()),
                 "value": {"type": char["name"], "value": char["value"]},
             }
-            for char in Characteristic.objects.filter(name__contains=val).values(
-                "name", "value"
-            )
+            for char in Characteristic.objects.filter(
+                value__unaccent__icontains=val
+            ).values("name", "value")
         ]
     )
     return schema
