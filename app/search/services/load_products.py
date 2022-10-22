75d1498ae8e7cd71c3a156e1be3b9fd23acb864b
Alexandr Karpov <alexandr.d.karpov@gmail.com>
2022-10-22 03:09:39 +0300

    fixed excel parser, processed to db

--- app/search/services/load_products.py
+++ app/search/services/load_products.py
@@ -22,32 +22,40 @@ def load():
 
 
 def load_excel():
-    df = pd.read_excel("data1.xlsx", sheet_name="Sheet1")
+    df = pd.read_excel("data.xlsx", sheet_name="Запрос1")
     for row in range(df.shape[0]):
-        print(df.iat[row, 0], df.iat[row, 1], df.iat[row, 2])
-        if Product.objects.filter(id=df.iat[row, 0]).exists():
-            Product.objects.filter(id=df.iat[row, 0]).delete()
-        product = Product(id=df.iat[row, 0])
-        product.name = df.iat[row, 1]
-        category = Category.objects.get_or_create(name=df.iat[row, 2])[0]
-        product.category = category
-        product.save()
-        for cat in literal_eval(df.iat[row, 4]):
-            try:
-                if "Unit" in cat:
-                    ProductUnitCharacteristic.objects.get_or_create(
-                        characteristic=UnitCharacteristic.objects.get_or_create(
-                            name=cat["Name"], value=cat["Value"], unit=cat["Unit"]
-                        )[0],
-                        product=product,
-                    )
-                else:
-                    ProductCharacteristic.objects.get_or_create(
-                        characteristic=Characteristic.objects.get_or_create(
-                            name=cat["Name"], value=cat["Value"]
-                        )[0],
-                        product=product,
-                    )
-            except KeyError:
-                # Empty Value
-                continue
+        try:
+            print(df.iat[row, 0], df.iat[row, 1], df.iat[row, 2])
+            if Product.objects.filter(id=df.iat[row, 0]).exists():
+                Product.objects.filter(id=df.iat[row, 0]).delete()
+            product = Product(id=df.iat[row, 0])
+            product.name = df.iat[row, 1]
+            category = Category.objects.get_or_create(name=df.iat[row, 2])[0]
+            product.category = category
+            product.save()
+            if df.iat[row, 4]:
+                for cat in literal_eval(df.iat[row, 4]):
+                    try:
+                        if "Unit" in cat:
+                            ProductUnitCharacteristic.objects.get_or_create(
+                                characteristic=UnitCharacteristic.objects.get_or_create(
+                                    name=cat["Name"],
+                                    value=cat["Value"],
+                                    unit=cat["Unit"],
+                                )[0],
+                                product=product,
+                            )
+                        else:
+                            ProductCharacteristic.objects.get_or_create(
+                                characteristic=Characteristic.objects.get_or_create(
+                                    name=cat["Name"], value=cat["Value"]
+                                )[0],
+                                product=product,
+                            )
+                    except KeyError:
+                        # Empty Value
+                        continue
+        except BaseException:
+            # malformed node or string: nan \ duplicate key
+            print("СКОРОСШИВАТЕЛЬ")
+            continue
