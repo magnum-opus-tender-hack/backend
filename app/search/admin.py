d74c515f4e5290c3deb9cf8d1d6b41425b6a1740
Alexandr Karpov <alexandr.d.karpov@gmail.com>
2022-10-23 08:33:25 +0300

    updated autocomplete, minor changes

--- app/search/admin.py
+++ app/search/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from search.models import (
+    Characteristic,
+    UnitCharacteristic,
+    Category,
+    Product,
+    ProductCharacteristic,
+    ProductUnitCharacteristic,
+)
+
 # Register your models here.
+
+
+admin.site.register(Characteristic)
+admin.site.register(UnitCharacteristic)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductCharacteristic)
+admin.site.register(ProductUnitCharacteristic)
--- app/search/services/autocomplete_schema.py
+++ app/search/services/autocomplete_schema.py
@@ -12,49 +12,67 @@ def autocomplete_schema(val: str, exclude: List[Dict]):
         schema.extend(
             [
                 {
-                    "coordinate": cat["name"].replace("ё", "е").lower().index(val.lower()),
+                    "coordinate": cat["name"]
+                    .replace("ё", "е")
+                    .lower()
+                    .index(val.lower()),
                     "value": {"type": "Category", "value": cat["name"]},
                 }
-                for cat in Category.objects.filter(name__unaccent__icontains=val)[
-                    :10
-                ].values("name")
+                for cat in (
+                    Category.objects.filter(name__unaccent__istartswith=val)
+                    | Category.objects.filter(name__unaccent__icontains=val)
+                )
+                .distinct()[:10]
+                .values("name")
             ]
         )
     if not name_exclude:
         schema.extend(
             [
                 {
-                    "coordinate": product["name"].replace("ё", "е").lower().index(val.lower()),
+                    "coordinate": product["name"]
+                    .replace("ё", "е")
+                    .lower()
+                    .index(val.lower()),
                     "value": {
                         "type": "Name",
                         "value": product["name"],
                     },
                 }
-                for product in Product.objects.filter(name__unaccent__icontains=val)[
-                    :30
-                ].values("name")
+                for product in (
+                    Product.objects.filter(name__unaccent__istartswith=val)
+                    | Product.objects.filter(name__unaccent__icontains=val)
+                )
+                .distinct()[:30]
+                .values("name")
             ]
         )
     schema.extend(
         [
             {
-                "coordinate": char["value"].replace("ё", "е").lower().index(val.lower()),
+                "coordinate": char["value"]
+                .replace("ё", "е")
+                .lower()
+                .index(val.lower()),
                 "value": {"type": char["name"], "value": char["value"]},
             }
-            for char in Characteristic.objects.filter(value__unaccent__icontains=val)[
-                :20
-            ].values("name", "value")
+            for char in (
+                Characteristic.objects.filter(value__unaccent__istartswith=val)
+                | Characteristic.objects.filter(value__unaccent__icontains=val)
+            )
+            .distinct()[:20]
+            .values("name", "value")
         ]
     )
     schema.extend(
         [
             {
                 "coordinate": char["name"].lower().replace("ё", "е").index(val.lower()),
-                "value": {
-                    "type": char["name"] + "_numeric",
-                    "value": char["name"]
-                }
-            } for char in UnitCharacteristic.objects.filter(name__unaccent__icontains=val)[:20].values("name", "value")
+                "value": {"type": char["name"] + "_numeric", "value": char["name"]},
+            }
+            for char in UnitCharacteristic.objects.filter(
+                name__unaccent__icontains=val
+            )[:20].values("name", "value")
         ]
     )
     return schema
